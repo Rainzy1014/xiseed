bcd30b7cc4b1b1a76ed3bb8fc023fece149e99c8
rainzy1014 <1617234498@qq.com>
2025-06-24 22:18:33 +0800

    v1.4

---
+++ frontend/src/views/landing/HomePage.vue
@@ -0,0 +1,396 @@
+<template>
+  <div id="home">
+    <Header></Header>
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+      </ol>
+
+      <!-- Wrapper for slides -->
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+          <img src="../../assets/landing/img/banner_1.jpg" alt="banner1" />
+          <div class="carousel-caption">...</div>
+        </div>
+        <div class="item">
+          <img src="../../assets/landing/img/banner_2.jpg" alt="banner2" />
+          <div class="carousel-caption">...</div>
+        </div>
+      </div>
+
+      <!-- Controls -->
+      <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+    <Footer></Footer>
+    <GoTop></GoTop>
+  </div>
+</template>
+<script>
+import Header from "./Header.vue";
+import Footer from "./Footer.vue";
+import GoTop from "./GoTop.vue";
+import "swiper/dist/css/swiper.min.css";
+import Swiper from "swiper";
+import { WOW } from 'wowjs';
+export default {
+  name: "HomePage",
+  components: {
+    Header,
+    Footer,
+    GoTop
+  },
+  mounted() {
+    new Swiper(".swiper-container", {
+      loop: true, // 循环模式选项
+      effect: 'fade',
+      //自动播放
+      autoplay: {
+        delay: 3000,
+        stopOnLastSlide: false,
+        disableOnInteraction: false
+      },
+      // 如果需要分页器
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+      },
+      // 如果需要导航按钮
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+      },
+      // 延迟加载
+      lazy: {
+        loadPrevNext: true
+      },
+      observer: true, //修改swiper自己或子元素时，自动初始化swiper
+      observeParents: true //修改swiper的父元素时，自动初始化swiper
+    });
+    /* customer-swiper */
+    new Swiper(".customer-swiper", {
+      loop: true, // 循环模式选项
+      slidesPerView: 3,
+      //自动播放
+      autoplay: {
+        delay: 3000,
+        stopOnLastSlide: false,
+        disableOnInteraction: false
+      },
+      // 如果需要前进后退按钮
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+      },
+      observer: true, //修改swiper自己或子元素时，自动初始化swiper
+      observeParents: true //修改swiper的父元素时，自动初始化swiper
+    });
+    /* wowjs动画 */
+    var wow = new WOW({
+      boxClass: 'wow',
+      animateClass: 'animated',
+      offset: 0,
+      mobile: true,
+      live: true
+    })
+    wow.init();
+  }
+};
+</script>
+<style scoped>
+@import "../../assets/landing/css/bootstrap.min.css";
+@import "../../assets/landing/css/reset.min.css";
+#home {
+  width: 100%;
+}
+
+/* 轮播图 */
+#carousel-example-generic {
+  height: 600px;
+}
+#carousel-example-generic .carousel-inner img {
+  width: 100%;
+  height: 100%;
+}
+#carousel-example-generic .carousel-inner {
+  position: relative;
+}
+#carousel-example-generic .carousel-caption {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 999999999;
+  width: 100%;
+  height: 100%;
+  color: #fff;
+  background: rgba(51, 51, 51, 0.534);
+  text-align: center;
+  line-height: 80px;
+}
+#carousel-example-generic .carousel-caption > h1{
+  font-size: 50px;
+  margin-top: 12%;
+}
+#carousel-example-generic .carousel-caption > p{
+  font-size: 20px;
+  margin-top: 1%;
+  font-weight: 700;
+}
+/* 大数据管理系统 */
+#bigData {
+  padding: 100px;
+  transition: all ease 0.6s;
+  box-sizing: border-box;
+}
+#bigData .bigData-title {
+  padding-bottom: 10px;
+  border-bottom: 1px solid #ccc;
+}
+#bigData p {
+  font-size: 14px;
+  color: #333;
+  line-height: 2rem;
+}
+#bigData .bigData-device {
+  margin: 50px 0 20px;
+}
+
+/* 您身边的IT专家 */
+#contactUs {
+  color: #fff;
+  height: 400px;
+  background: url("../assets/img/contact_us_bg.jpg") 0 0 no-repeat;
+  background-size: 100% 100%;
+  transition: all ease 0.6s;
+}
+#contactUs .contactUs-container {
+  padding-top: 50px;
+}
+#contactUs .contactUs-container button {
+  width: 300px;
+  height: 50px;
+  margin-top: 40px;
+}
+#contactUs .contactUs-container .contactUs-contactWay span {
+  display: inline-block;
+  width: 48px;
+  height: 48px;
+  margin: 30px;
+}
+#contactUs .contactUs-container .contactUs-contactWay span:nth-of-type(1) {
+  background: url("../assets/img/weixin.png") 0 0 no-repeat;
+  background-size: 100% 100%;
+}
+#contactUs .contactUs-container .contactUs-contactWay span:nth-of-type(2) {
+  background: url("../assets/img/weibo.png") 0 0 no-repeat;
+  background-size: 100% 100%;
+}
+#contactUs .contactUs-container .contactUs-contactWay span:nth-of-type(3) {
+  background: url("../assets/img/twitter.png") 0 0 no-repeat;
+  background-size: 100% 100%;
+}
+
+/* 客户评价 */
+#customer {
+  padding: 50px 0;
+  box-sizing: border-box;
+  background: #efefef;
+  transition: all ease 0.6s;
+}
+#customer .customer-title {
+  font-size: 30px;
+  color: rgb(102, 102, 102);
+  margin: 0 0 30px;
+}
+#customer .customer-block {
+  background: #fff;
+  padding: 30px;
+}
+#customer .customer-logo img {
+  width: 94px;
+  height: 94px;
+  border: 1px solid #ccc;
+}
+#customer .customer-yh img {
+  width: 34px;
+  height: 34px;
+}
+#customer .customer-content1 {
+  padding-bottom: 20px;
+  border-bottom: 1px solid #0ce9f1;
+}
+#customer .customer-content2 {
+  padding-top: 20px;
+}
+/* 为什么选择我们 */
+#whyChooseUs {
+  padding: 100px;
+}
+#whyChooseUs .whyChooseUs-title {
+  margin-bottom: 50px;
+}
+#whyChooseUs .whyChooseUs-title p:nth-of-type(1) {
+  font-size: 25px;
+  font-weight: 500;
+}
+#whyChooseUs .whyChooseUs-title p:nth-of-type(2) {
+  font-size: 14px;
+}
+#whyChooseUs .server-block {
+  padding: 50px 20px;
+  border: 1px solid #ccc;
+  border-bottom: 5px solid #ccc;
+}
+#whyChooseUs .server-block img {
+  width: 48px;
+  height: 48px;
+}
+#whyChooseUs .server-block > p {
+  font-size: 20px;
+  margin: 30px 0;
+}
+#whyChooseUs .server-block > div {
+  color: #ccc;
+}
+/* 媒体查询（手机） */
+@media screen and (max-width: 768px) {
+  #carousel-example-generic {
+    height: 200px;
+  }
+  #bigData {
+    padding: 30px;
+  }
+  #bigData .bigData-title {
+    font-size: 20px;
+  }
+  #bigData .bigData-device {
+    font-size: 20px;
+    margin: 10px 0 10px;
+  }
+  #contactUs {
+    height: 200px;
+    transition: all ease 0.6s;
+  }
+  #contactUs .contactUs-container {
+    padding-top: 0;
+  }
+  #contactUs .contactUs-container h1 {
+    font-size: 25px;
+  }
+  #contactUs .contactUs-container h3 {
+    font-size: 18px;
+  }
+  #contactUs .contactUs-container button {
+    width: 200px;
+    height: 30px;
+    margin-top: 20px;
+  }
+  #contactUs .contactUs-container .contactUs-contactWay span {
+    display: inline-block;
+    width: 28px;
+    height: 28px;
+    margin: 10px;
+  }
+  #customer {
+    padding: 30px 0;
+    box-sizing: border-box;
+    background: #fff;
+  }
+  #customer .customer-title {
+    font-size: 16px;
+    font-weight: bold;
+  }
+  #customer .customer-logo img {
+    width: 48px;
+    height: 48px;
+  }
+  #customer .customer-block {
+    padding: 30px;
+  }
+  #customer .customer-block > div {
+    padding: 30px 0;
+  }
+  #whyChooseUs {
+    padding: 20px 0;
+    transition: all ease 0.6s;
+  }
+  #whyChooseUs .whyChooseUs-title p:nth-of-type(1) {
+    font-size: 20px;
+    font-weight: 700;
+  }
+  #whyChooseUs .whyChooseUs-title p:nth-of-type(2) {
+    font-size: 12px;
+  }
+  #whyChooseUs .server-block {
+    padding: 50px 0;
+    border: 1px solid #ccc;
+    border-bottom: 5px solid #ccc;
+  }
+  #whyChooseUs .server-block img {
+    width: 48px;
+    height: 48px;
+  }
+  #whyChooseUs .server-block > p {
+    font-size: 20px;
+    margin: 30px 0;
+  }
+  #whyChooseUs .server-block > div {
+    color: #ccc;
+  }
+}
+
+/* 媒体查询（平板） */
+@media screen and (min-width: 768px) and (max-width: 996px) {
+  #carousel-example-generic {
+    height: 400px;
+  }
+  #bigData {
+    padding: 60px;
+  }
+  #bigData .bigData-title {
+    font-size: 30px;
+  }
+  #bigData .bigData-device {
+    font-size: 30px;
+    margin: 30px 0 15px;
+  }
+  #contactUs {
+    height: 300px;
+  }
+  #contactUs .contactUs-container {
+    padding-top: 50px;
+  }
+  #contactUs .contactUs-container h1 {
+    font-size: 30px;
+  }
+  #contactUs .contactUs-container h3 {
+    font-size: 20px;
+  }
+  #contactUs .contactUs-container button {
+    width: 300px;
+    height: 50px;
+    margin-top: 30px;
+  }
+  #contactUs .contactUs-container .contactUs-contactWay span {
+    display: inline-block;
+    width: 32px;
+    height: 32px;
+    margin: 15px;
+  }
+  #customer .customer-title {
+    font-size: 24px;
+  }
+  #whyChooseUs {
+    padding: 20px 0;
+  }
+}
+</style>
+
